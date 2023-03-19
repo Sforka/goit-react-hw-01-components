cd226b67db9a48631227a60642a7a824d823cc8a
Sforka <warlock20115@gmail.com>
2023-03-19 19:27:42 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import user from './user.json';
-import data from './data.json';
-import friends from './friends.json';
-import transactions from './transactions.json';
-import { Profile } from './components/profile/Profile.jsx';
-import { Statistics } from './components/statistics/statistics.jsx';
-import { FriendList } from './components/friendList/friendList.jsx';
-import { TransactionHistory } from './components/transactionHistory/transactionHistory.jsx';
+// import user from './user.json';
+// import data from './data.json';
+// import friends from './friends.json';
+// import transactions from './transactions.json';
+// import { Profile } from './components/profile/Profile.jsx';
+// import { Statistics } from './components/statistics/statistics.jsx';
+// import { FriendList } from './components/friendList/friendList.jsx';
+// import { TransactionHistory } from './components/transactionHistory/transactionHistory.jsx';
 import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
