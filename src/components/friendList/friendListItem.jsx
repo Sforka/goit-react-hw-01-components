903d8a3f63fc436b274fb0dbff619166451ff1b5
Sforka <warlock20115@gmail.com>
2023-03-20 22:22:54 +0200

    1
    1

--- src/components/friendList/friendListItem.jsx
+++ src/components/friendList/friendListItem.jsx
@@ -4,27 +4,9 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.friendList_item}>
       {isOnline === true ? (
-        <span
-          className={css.friendList_status}
-          style={{
-            backgroundColor: 'green',
-            width: 10,
-            height: 10,
-            display: 'block',
-            borderRadius: 50,
-          }}
-        ></span>
+        <span className={css.friendList_green}></span>
       ) : (
-        <span
-          className={css.friendList_status}
-          style={{
-            backgroundColor: 'red',
-            width: 10,
-            height: 10,
-            display: 'block',
-            borderRadius: 50,
-          }}
-        ></span>
+        <span className={css.friendList_red}></span>
       )}
       <img
         className={css.friendList_avatar}
